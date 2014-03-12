e1d8c969fdecc3313549e8345a3fee0e9cfb0d93
ruudvanderweijde <ruudvanderweijde@gmail.com>
2014-03-12 12:43:22 +0100

    generated test files based on cwi-swat parser, will be used when refactoring the parser

---
+++ test/Rascal/AST2RascalTest.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: ruud
+ * Date: 12/03/14
+ * Time: 09:16
+ */
+
+namespace Rascal;
+
+
+class AST2RascalTest extends \PHPUnit_Framework_TestCase {
+
+}
+ 
